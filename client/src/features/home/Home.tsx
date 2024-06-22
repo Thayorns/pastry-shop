caf649a1b81d0902955455d86a72e9a6b0d4838b
valentine <thayornswordsman@gmail.com>
2024-06-22 03:12:52 +0300

    крем и корж. роут ДОМ. рефакторинг кода

--- client/src/features/home/Home.tsx
+++ client/src/features/home/Home.tsx
@@ -29,30 +29,21 @@ const Home: React.FC = () => {
 
     const horizonAnchors = [ 'Торты', 'Выпечка', 'Десерты', 'Напитки', 'Сендвичи', 'Салаты' ];
 
-    const chapterCake = result.filter(obj => obj.chapter === 'Торты');
-    const chapterBakery = result.filter(obj => obj.chapter === 'Выпечка');
-    const chapterDesserts = result.filter(obj => obj.chapter === 'Десерты');
-    const chapterDrinks = result.filter(obj => obj.chapter === 'Напитки');
-    const chapterSandwitches = result.filter(obj => obj.chapter === 'Сендвичи');
-    const chapterSalads = result.filter(obj => obj.chapter === 'Салаты');
+    const chapterMap = horizonAnchors.reduce((accumulator, chapter) => {
+        accumulator[chapter] = result.filter(obj => obj.chapter === chapter);
+        return accumulator;
+    }, {} as Record<string, ResultResponse[]>);
 
     const mapFunction = (arr: ResultResponse[]) => {
         const result = arr.map((obj, index) => (
-            <div className="single-card-inner">
-                <img key={index} src={require(`../../../../product-photos/${obj.photo}`)} alt=""/>
+            <div className="single-card-inner" key={index}>
+                <img src={require(`../../../../product-photos/${obj.photo}`)} alt=""/>
                 <p>{obj.title}</p>
                 <span>{obj.price} руб.</span>
             </div>    
         ));
         return result;
     };
-    
-    let filteredCakes = mapFunction(chapterCake),
-    filteredBakery = mapFunction(chapterBakery),
-    filteredDeserts = mapFunction(chapterDesserts),
-    filteredDrinks = mapFunction(chapterDrinks),
-    filteredSandwitches = mapFunction(chapterSandwitches),
-    filteredSalads = mapFunction(chapterSalads);
 
     return (
         <>
@@ -60,7 +51,6 @@ const Home: React.FC = () => {
             
             {isSuccess && (
                 <div className="home-div">
-
                     <div className="horizontal-filter">
                         {horizonAnchors.map((anchor, index) => 
                             <AnchorLink key={index} offset={() => 100} href={`#${anchor}`}>
@@ -69,36 +59,15 @@ const Home: React.FC = () => {
                                     {anchor}
                                 </span>
                             </AnchorLink>
-                            
                         )}
                     </div>
-
                     <div className="vertical-filter">
-                        
-                        <div id="Торты" className="cards-wrapper">
-                            <h3>ТОРТЫ <RightOutlined className="title-arrow"/></h3>
-                            {filteredCakes}
-                        </div>
-                        <div  id="Выпечка" className="cards-wrapper">
-                            <h3>ВЫПЕЧКА <RightOutlined className="title-arrow"/></h3>
-                            {filteredBakery}
-                        </div>
-                        <div  id="Десерты" className="cards-wrapper">
-                            <h3>ДЕСЕРТЫ <RightOutlined className="title-arrow"/></h3>
-                            {filteredDeserts}
-                        </div>
-                        <div id="Напитки" className="cards-wrapper">
-                            <h3>НАПИТКИ <RightOutlined className="title-arrow"/></h3>
-                            {filteredDrinks}
-                        </div>
-                        <div id="Сендвичи" className="cards-wrapper">
-                            <h3>СЕНДВИЧИ <RightOutlined className="title-arrow"/></h3>
-                            {filteredSandwitches}
-                        </div>
-                        <div id="Салаты" className="cards-wrapper">
-                            <h3>САЛАТЫ <RightOutlined className="title-arrow"/></h3>
-                            {filteredSalads}
-                        </div>
+                        {horizonAnchors.map((chapter, index) => (
+                            <div id={chapter} className="cards-wrapper" key={index}>
+                                <h3>{chapter.toUpperCase()}<RightOutlined className="title-arrow"/></h3>
+                                {mapFunction(chapterMap[chapter])}
+                            </div>
+                        ))}
                     </div>
                 </div>
             )}
