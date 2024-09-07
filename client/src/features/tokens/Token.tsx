f7ff7b4f8a0f40ce10be6491824d6af8ed4a3bbe
valentine <thayornswordsman@gmail.com>
2024-09-07 22:54:44 +0300

    refactor Token component - redirect to frontend at /api/activate/:token && deleted useGetTokenQuery for its useless

--- client/src/features/tokens/Token.tsx
+++ client/src/features/tokens/Token.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useGetTokenQuery } from "../api/apiSlice";
+// import { useGetTokenQuery } from "../api/apiSlice";
 import { useParams } from 'react-router-dom';
 import { Spin, Result } from "antd";
 
@@ -10,35 +10,21 @@ import '../../app/styles/vars.css'
 
 const Token: React.FC = () => {
     const { token } = useParams<{ token: string }>();
-    const { data, isLoading, isSuccess, isError } = useGetTokenQuery(token);
+    // const { data, isLoading, isSuccess, isError } = useGetTokenQuery(token);
 
-    type TokenResponse = {
-        message: string;
-    };
-    const tokenResponse = data as TokenResponse;
+    // type TokenResponse = {
+    //     message: string;
+    // };
+    // const tokenResponse = data as TokenResponse;
 
     return (
-        <>  
-            {isLoading && (
-                <Spin/>
-            )}
-
-            {isSuccess && tokenResponse && (
-                <div className="token-div">
-                    <Result
-                        status="success"
-                        title="Ваш аккаунт успешно активирован!"
-                        subTitle="Войдите под своим логином и паролем."
-                    />
-                </div>
-            )}
-
-            {isError && (
-                <div>
-                    <Result status="500" title="Ошибка" subTitle="Простите, что-то пошло не так." />
-                </div>
-            )}
-        </>
-    )
+        <div className="token-div">
+            <Result
+                status="success"
+                title="Ваш аккаунт успешно активирован!"
+                subTitle="Теперь вы можете войти под своим логином и паролем."
+            />
+        </div>
+    );
 }
 export default Token;
